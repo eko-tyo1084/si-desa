bde7f3d94bd7549fa1b8e353af652142e71902fe
eko-tyo1084 <75320667+eko-tyo1084@users.noreply.github.com>
2021-01-12 21:33:18 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Desa Perk. Limau Manis</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logobb.png"></img></p>
+                          <p class="label-title" align="center"><strong>Desa Perkebunan Limau Manis</strong></p>
+                          <p class="label-title" align="center"><strong>Kecamatan Lima Puluh, Kabupaten Batu Bara, Sumatera Utara</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Desa Perk. Limau Manis.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data penduduk, dan data dusun.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data penduduk, dan data dusun.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
